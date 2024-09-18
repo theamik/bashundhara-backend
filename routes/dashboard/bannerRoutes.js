b438049f62cdb633a51c31bc967c4d8151a7a636
theamik <118734316+theamik@users.noreply.github.com>
2024-09-18 12:16:20 +0600

    Update bannerRoutes.js

--- routes/dashboard/bannerRoutes.js
+++ routes/dashboard/bannerRoutes.js
@@ -4,6 +4,6 @@ const bannerController = require('../../controllers/dashboard/bannerController')
 
 router.post('/banner-add', authMiddleware, bannerController.add_banner)
 router.get('/banner-get/:productId', authMiddleware, bannerController.get_banner)
-router.get('/banners-get', authMiddleware, bannerController.get_banners)
+router.get('/banners-get', bannerController.get_banners)
 
 module.exports = router
